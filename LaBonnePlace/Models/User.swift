8ee198b966bb018cb16823b21dda7b05c5d97c6c
mazurekevin <kevin.mazure.km@gmail.com>
2025-03-21 21:30:48 +0100

    Créaction architecture MVVM

---
+++ LaBonnePlace/Models/User.swift
@@ -0,0 +1,25 @@
+//
+//  User.swift
+//  LaBonnePlace
+//
+//  Created by teamdoc on 21/03/2025.
+//
+
+import Foundation
+import SwiftyJSON
+struct User: Identifiable {
+    var id: Int
+    var firstname: String
+    var lastname: String
+    var mail: String
+    var password: String
+    
+
+    init(json: JSON) {
+        id = json["id"].intValue
+        firstname = json["firstname"].stringValue
+        lastname = json["lastname"].stringValue
+        mail = json["mail"].intValue
+        password = json["password"].stringValue
+    }
+}
